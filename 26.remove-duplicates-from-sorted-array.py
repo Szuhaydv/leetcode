b7ede00539253a5dbc947cc59a5f8db9a60c3eb3
szuhaydv <szuhaydv@gmail.com>
2024-10-04 21:59:31 +0200

    problem 26 - imp: no need for dict to keep track

--- 26.remove-duplicates-from-sorted-array.py
+++ 26.remove-duplicates-from-sorted-array.py
@@ -1,13 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        uniques = {}
-        last_sorted_id = -1
-        for i, val in enumerate(nums):
+        # No need for dict to store uniques
+        # you are comparing to the last fixed item
+        last_sorted_id = 0
+        for i in range(1, len(nums)):
                 # could use convenience function: nums.remove(nums[i])
                 # A bit more revealing:
-            if val not in uniques:
-                nums[last_sorted_id + 1] = val
+            if nums[i] != nums[last_sorted_id]:
+                nums[last_sorted_id + 1] = nums[i]
                 last_sorted_id += 1
-                uniques[val] = True
-        return len(uniques)
+        return last_sorted_id + 1
 
